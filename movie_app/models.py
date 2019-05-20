944f9b25435e6255980c0e7932272a0254c4c005
Anuj Singh <44033673+anuj920@users.noreply.github.com>
2019-05-20 20:46:35 +0530

    Add files via upload

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 from django.urls import reverse
 from django.core.validators import MinValueValidator, MaxValueValidator
 
